c9b83d748bc10a12d4abb3773a4cc25f2f55a8c7
Tyler Grubb <grubber1618@gmail.com>
2022-11-07 13:07:38 -0600

    feat: setup basic form and simple seed generation

---
+++ src/components/SeedGenerator/Form.svelte
@@ -0,0 +1,26 @@
+<script>
+  import short from 'short-uuid';
+
+  let seed = '';
+  function onRandomize() {
+    seed = short.generate().substring(0, 10).toUpperCase();
+  }
+</script>
+
+<label>
+  Searchable Treasure Locations:&nbsp;
+  <input type=number name="number-locations" value={10} />
+</label>
+<br /><br />
+<label>
+  Number of Treasures:&nbsp;
+  <input type=number name="number-treasures" value={5} />
+</label>
+<br /><br />
+<label>
+  Seed:&nbsp;
+  <input type=text name="seed" bind:value={seed} />
+</label>
+<button on:click={onRandomize}>
+  Randomize Seed
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+  import SeedGeneratorForm from '../components/SeedGenerator/Form.svelte';
+</script>
+
 <h1>Pokemon Crystal Hidden Rivals Treasure Hunt</h1>
 
+<SeedGeneratorForm />
