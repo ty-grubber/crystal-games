94d6e72d7a65c60f7a02bfd535c86a77bdab8b85
Tyler Grubb <grubber1618@gmail.com>
2022-11-07 12:54:12 -0600

    feat: add some initial treasure entries

---
+++ src/constants/treasures.js
@@ -0,0 +1,76 @@
+export const Treasures = [
+  {
+    description: 'Talk to the NPC who lives in the house by Tohjo Falls.',
+    hintOpts: [
+      'You\'ll find you\'ll treasure a nice chat with a friend.',
+      'Not all treasure is found outside.',
+    ]
+  },
+  {
+    description: 'Collect the hidden item in the water on Cycling Road',
+    hintOpts: [
+      'You\'ll need to go for a ride to find this treasure.',
+      'Some treasures aren\'t immediately obvious.',
+    ]
+  },
+  {
+    description: 'Collect the items from the trees in Pewter City.',
+    hintOpts: [
+      'You can find treasure by reaping some fruity rewards.',
+      'Some treasures come in pairs, or was it pears?',
+    ]
+  },
+  {
+    description: 'Battle the non-female trainer in Janine\'s Gym.',
+    hintOpts: [
+      'Treasure is the reward for success on the battlefield.',
+      'You will find treasure hidden in a Gym.',
+    ]
+  },
+  {
+    description: 'Rescue DJ Mary.',
+    hintOpts: [
+      'The reward of helping someone in need is all the treasure you need.',
+      'Treasure is the reward for success on the battlefield.',
+    ]
+  },
+  {
+    description: 'Collect the Cerulean Cave hidden item.',
+    hintOpts: [
+      'You\'ll need to calm the seas to find some treasures.',
+      'Some treasures aren\'t immediately obvious.',
+    ]
+  },
+];
+
+
+
+/* Remaining Treasures to add (each one is on a separate line)
+
+Collect both berries on Route 46.	You can find treasure by reaping some fruity rewards.	Some treasures come in pairs, or was it pears?
+Buy something from the Department Store in Celadon City.	Some treasures can only be bought.	Not all treasure is found outside.
+Dispel a Whirlpool in the Dragon's Den.	You'll need to hang ten to find some treasures.	You'll need to calm the seas to find some treasures.
+Inspect two adjacent trash cans in Lt. Surge's Gym	You will find treasure hidden in a Gym.	One person's trash is another person's treasure
+Battle the optional trainer in Morty's Gym.	Treasure is the reward for success on the battlefield.	You will find treasure hidden in a Gym.
+Check up on the captain of the S.S. Anne	A boat has already salvaged a sunken treasure	You'll find you'll treasure a nice chat with a friend.
+Collect the item behind the Cut Tree connected to the dock on Route 12.	Sometimes the best treasures are just found lying on the ground.	You will need to cut down some obstacles in your way to find the treasure you seek.
+Destroy the rocks (3) in the Dark Cave	Sometimes you need a bright light to find treasure	Some treasures are found amongst the rubble.
+Catch a Pokémon in the Grass on Route 10.	New friends are always something to treasure.	Search the tall grass to find some treasure.
+Read the sign beside Snorlax in Vermillion City	New friends are always something to treasure.	You'll need your reading glasses to find this treasure.
+Receive the gift from the Roar Guy on Route 32.	You will need to cut down some obstacles in your way to find the treasure you seek.	Sometimes treasure is just given to us willingly.
+Receive a Pokémon from Bill.	New friends are always something to treasure.	Sometimes treasure is just given to us willingly.
+Collect the item beyond the north-western pond in Violet City.	Sometimes the best treasures are just found lying on the ground.	You'll need to hang ten to find some treasures.
+Collect all 3 Apricorns on Route 42.	You can find treasure by reaping some fruity rewards.	You will need to cut down some obstacles in your way to find the treasure you seek.
+Talk to the NPC who lives north of the Cycling Road.	You'll find you'll treasure a nice chat with a friend.	Not all treasure is found outside.
+Heal Moomoo.	The reward of helping someone in need is all the treasure you need.	You can find treasure by reaping some fruity rewards.
+Destroy the rocks in Ice Path	With great strength comes great treasure.	Some treasures are found amongst the rubble.
+Buy something from the Pokémart in Olivine City.	Some treasures can only be bought.	Not all treasure is found outside.
+Heal Amphy.	The reward of helping someone in need is all the treasure you need.	Not all treasure is found outside.
+Push a Strength Boulder down the useless hole in Claire's Gym.	With great strength comes great treasure.	You will find treasure hidden in a Gym.
+Collect the item behind the Strength Boulder in the Burned Tower.	With great strength comes great treasure.	A treasure is reborn from the ashes.
+Inspect the trash can on the S.S. Anne in the sailor's quarters	A boat has already salvaged a sunked treasure	One person's trash is another person's treasure
+Battle the trainer in the grass on Route 18.	Treasure is the reward for success on the battlefield.	Search the tall grass to find some treasure.
+Buy an item from the PokeMart in Fuschia City	Some treasures can only be bought.	Not all treasure is found outside.
+Dispel a Whirlpool on Route 41.	You'll need to hang ten to find some treasures.	You'll need to calm the seas to find some treasures.
+Flash the ruins to read the Unown message.	Sometimes you need a bright light to find treasure.	You'll need your reading glasses to find this treasure.
+*/
