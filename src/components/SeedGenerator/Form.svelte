0ea92c80afa566b58c10bac76cf5c0d9ad00a2d7
Tyler Grubb <grubber1618@gmail.com>
2022-11-07 13:26:44 -0600

    feat: handle form submission

--- src/components/SeedGenerator/Form.svelte
+++ src/components/SeedGenerator/Form.svelte
@@ -1,20 +1,36 @@
 <script>
   import short from 'short-uuid';
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
 
   let seed = '';
+  let locations = 10;
+  let treasures = 5;
+
   function onRandomize() {
     seed = short.generate().substring(0, 10).toUpperCase();
+  }
+
+  function onStartClick() {
+    dispatch('startGame', {
+      gameOptions: {
+        locations,
+        treasures,
+        seed,
+      }
+    })
   }
 </script>
 
 <label>
   Searchable Treasure Locations:&nbsp;
-  <input type=number name="number-locations" value={10} />
+  <input type=number name="number-locations" bind:value={locations} />
 </label>
 <br /><br />
 <label>
   Number of Treasures:&nbsp;
-  <input type=number name="number-treasures" value={5} />
+  <input type=number name="number-treasures" bind:value={treasures} />
 </label>
 <br /><br />
 <label>
@@ -23,4 +39,9 @@
 </label>
 <button on:click={onRandomize}>
   Randomize Seed
+</button>
+<br /><br />
+
+<button on:click={onStartClick} disabled={!seed}>
+  Start Game!
 </button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
 <script>
   import SeedGeneratorForm from '../components/SeedGenerator/Form.svelte';
+
+
+  /**
+	 * @param {{ detail: { gameOptions: {
+    * locations: string;
+    * treasures: string;
+    * seed: string; };
+   * }; }} event
+	 */
+  function handleStartGame(event) {
+    const {locations, treasures, seed} = event.detail.gameOptions;
+    alert(`Game Options: Locations: ${locations}, Treasures: ${treasures}, Seed: ${seed}`);
+  }
 </script>
 
 <h1>Pokemon Crystal Hidden Rivals Treasure Hunt</h1>
 
-<SeedGeneratorForm />
+<SeedGeneratorForm on:startGame={handleStartGame} />
