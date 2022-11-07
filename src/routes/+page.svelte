d7c3c8b09e80658fd99593c968a6527103d25945
Tyler Grubb <grubber1618@gmail.com>
2022-11-07 17:58:53 -0600

    feat: generate randomized list of rivals; allow treasures to be 0 to just play rivals

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script>
   import SeedGeneratorForm from '../components/SeedGenerator/Form.svelte';
-  import { randomizeArray } from '../lib/randomize';
+  import RIVALS from '../constants/rivals';
   import TREASURES from '../constants/treasures';
+  import { randomizeArray } from '../lib/randomize';
 
   /**
 	 * @type {any[]}
@@ -11,23 +12,31 @@
 	 * @type {String[]}
 	 */
   let treasureIds = [];
+  /**
+	 * @type {string | any[]}
+	 */
+  let selectedRivals = [];
 
 
   /**
 	 * @param {{ detail: { gameOptions: {
+    * rivals: string;
     * locations: string;
     * treasures: string;
     * seed: string; };
    * }; }} event
 	 */
   function handleStartGame(event) {
-    const {locations, treasures: numTreasures, seed} = event.detail.gameOptions;
+    const {rivals, locations, treasures: numTreasures, seed} = event.detail.gameOptions;
     const randomizedTreasures = randomizeArray(TREASURES, seed);
 
     const searchableLocations = randomizedTreasures.slice(0, parseInt(locations));
     treasureIds = searchableLocations.slice(0, parseInt(numTreasures)).map(location => location.id);
 
     treasureLocations = randomizeArray(searchableLocations, `${seed}${numTreasures}`);
+
+    const randomizedRivals = randomizeArray(RIVALS, seed);
+    selectedRivals = randomizedRivals.slice(0, parseInt(rivals));
   }
 </script>
 
@@ -36,11 +45,21 @@
 <SeedGeneratorForm on:startGame={handleStartGame} />
 <br /><br />
 
+{#if treasureLocations.length > 0}
 <ul>
   {#each treasureLocations as location (location.id) }
   <li class={treasureIds.includes(location.id) ? 'treasure' : 'not-treasure'}>{location.description}</li>
   {/each}
 </ul>
+{/if}
+
+{#if selectedRivals.length > 0}
+<ul>
+  {#each selectedRivals as rival (rival.id) }
+  <li class="rival">{rival.name}</li>
+  {/each}
+</ul>
+{/if}
 
 <style>
   .treasure {
@@ -48,5 +67,9 @@
   }
   .not-treasure {
     color: darkred;
+  }
+
+  .rival {
+    color: blueviolet;
   }
 </style>
