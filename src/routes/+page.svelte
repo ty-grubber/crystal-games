bdd1cdb1498a9df6d3c29989f9bc034ef28c1c89
Tyler Grubb <grubber1618@gmail.com>
2022-11-13 11:24:43 -0600

    feat: add how to play button and dialog on main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,7 +22,11 @@
   /**
    * @type {boolean}
    */
-  let dialogOpen = true;
+  let settingsDialogOpen = true;
+  /**
+   * @type {boolean}
+   */
+  let howToDialogOpen = false;
   /**
    * @type {string}
    */
@@ -51,7 +55,7 @@
     selectedRivals = randomizedRivals.slice(0, parseInt(rivals));
     // TODO: Should we auto-sort the rivals if it gets over a certain amount?
 
-    dialogOpen = false;
+    settingsDialogOpen = false;
     chosenSeed = seed;
   }
 
@@ -60,7 +64,12 @@
     selectedRivals = [];
     treasureLocations = [];
     chosenSeed = '';
-    dialogOpen = true;
+    settingsDialogOpen = true;
+    howToDialogOpen = false;
+  }
+
+  function openHowToDialog() {
+    howToDialogOpen = true;
   }
 </script>
 
@@ -68,13 +77,42 @@
 <Button color="primary" on:click={handleStartNewGame} variant="raised">
   <Label>Start New Game</Label>
 </Button>
+<Button color="secondary" on:click={openHowToDialog} variant="raised">
+  <Label>How To Play</Label>
+</Button>
 {#if chosenSeed}
   <span>Current seed: {chosenSeed}</span>
 {/if}
-<Dialog bind:open={dialogOpen} surface$style="width: 850px;">
+<Dialog bind:open={settingsDialogOpen} surface$style="width: 850px;">
   <Title id="settingsTitle">Tracker Settings</Title>
   <Content id="settingsContent">
     <SeedGeneratorForm on:startGame={handleStartGame} />
+  </Content>
+</Dialog>
+<Dialog bind:open={howToDialogOpen} slot="over" surface$style="height: 600px;">
+  <Title id="howToTitle">How To Play</Title>
+  <Content id="howToContent">
+    <p>When the page initially loads you will be presented with a dialog to input the settings for the tracker:</p>
+    <ul>
+      <li><b>Rivals To Defeat:</b> - The number of rivals you need to defeat to win Hidden Rivals. If 0, a list of rivals will not be generated.</li>
+      <li><b>Searchable Treasure Locations</b> - The number of possible locations on the map where treasures could be located. This number cannot be smaller than the <b>Number of Treasures</b>.</li>
+      <li><b>Number of Treasures</b> - The total number of treasures you need to find. If this and <b>Searchable Treasure Locations</b> are 0, a list of treasure locations will not be generated.</li>
+      <li><b>Seed</b> - A string used for randomization purposes. Type one of your choice or click the Randomize Seed button to have one generated for you.</li>
+    </ul>
+
+    <h3>Treasure Hunt</h3>
+
+    <p>Treasure Hunt requires you to explore Johto and Kanto to interact with specific NPCs, items or other interactive objects in order to find treasure. Each potential treasure location is listed under the Location column of the tracker. Find all treasures to win!</p>
+
+    <p>If you complete a task where a treasure could be found, check the box in the Found? column of the appropriate row. If there is a treasure there, the Result column will display "TREASURE!!!". If there is not a treasure there, the Result column will display a hint to another location where there is actually treasure. These hints will only double up if there are more non-treasure locations than treasure locations.</p>
+
+    <h3>Hidden Rivals</h3>
+
+    <p>Hidden Rivals is pretty straightforward. The tracker provides a list of trainers that you need to seek out and defeat in battle. When you have defeated all their Pokemon in battle, check the box in the Defeated? column. Check off all boxes to win!</p>
+
+    <p><b>Note:</b> it is recommended to play without randomizing trainer names and class names in Universal Pokemon Randomizer so the trainers in this list are easily verified when fighting them.</p>
+
+    <p><b>Note:</b> some rivals are missable due to various reasons (like plot or defeating a gym leader). These rivals are marked with an `*` so you are aware of this potential to miss them. This `*` assumes that the setting isn't on where all trainers are spinners, but does assume an item randomizer has been applied (so some missable trainers won't actually be missable if you are playing vanilla).</p>
   </Content>
 </Dialog>
 
