df8cd170d2bd24ea9088249c0da1324c65e37461
Tyler Grubb <grubber1618@gmail.com>
2022-12-27 11:10:59 -0600

    feat: randomize grid

--- src/routes/minesweeper/+page.svelte
+++ src/routes/minesweeper/+page.svelte
@@ -1,27 +1,69 @@
 <script>
+  import short from 'short-uuid';
+  import Button, { Label } from '@smui/button';
+  import TextField from '@smui/textfield';
   import { NATIONAL_DEX } from '../../constants/pokedex';
+	import { randomizeArray } from '$lib/randomize';
+
+  let seed = '';
+  let monList = NATIONAL_DEX;
+
+  function onRandomize() {
+    seed = short.generate().substring(0, 12).toUpperCase();
+  }
+
+  function onResetClick() {
+    seed = '';
+    monList = NATIONAL_DEX;
+  }
+
+  function onStartClick() {
+    monList = randomizeArray(NATIONAL_DEX, seed);
+  }
 </script>
 
-<div class="container">
+<div class="page">
   <h1>Pokémon Crystal Minesweeper</h1>
 
-  <div class="dex-grid">
-    {#each NATIONAL_DEX as pokemon, i (pokemon.id)}
-      <div class="dex-mon">
-        <img class="mon-icon" src={`/pokedex/${pokemon.id}.png`} alt={pokemon.name} />
-      </div>
-    {/each}
+  <div class="grid">
+    <div class="dex">
+      {#each monList as pokemon, i (pokemon.id)}
+        <div class="dex-mon">
+          <img class="mon-icon" src={`/pokedex/${pokemon.id}.png`} alt={pokemon.name} />
+        </div>
+      {/each}
+    </div>
+  </div>
+  <div class="options">
+    <div class="randomizer">
+      <TextField variant="outlined" bind:value={seed} label="Seed:" />
+      <Button color="secondary" on:click={onRandomize} variant="unelevated">
+        <Label>Randomize Seed</Label>
+      </Button>
+      <br /><br />
+      <Button color="primary" on:click={onStartClick} variant="unelevated">
+        <Label>Start Game!</Label>
+      </Button>
+      <Button color="secondary" on:click={onResetClick} variant="unelevated">
+        <Label>Reset Grid</Label>
+      </Button>
+    </div>
   </div>
 </div>
 
 <style>
-  .dex-grid {
+  .grid,
+  .options {
+    display: inline-flex;
+  }
+
+  .dex {
     display: flex;
     flex-wrap: wrap;
     max-width: 700px;
   }
 
-  .dex-grid > .dex-mon {
+  .dex > .dex-mon {
     max-width: calc(100% / 16);
   }
 
