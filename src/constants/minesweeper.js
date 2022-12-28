c612e0b1135583298288d7951a3b08f8d2155da3
Tyler Grubb <grubber1618@gmail.com>
2022-12-28 14:39:21 -0600

    feat: add status and mine constants

---
+++ src/constants/minesweeper.js
@@ -0,0 +1,18 @@
+const STATUS = {
+  EMPTY: 'unknown',
+  FLAGGED: 'flagged',
+  SAFE: 'safe',
+  SEEN: 'seen',
+  OWNED: 'owned',
+  MINED: 'mined',
+  EXPLODED: 'exploded',
+};
+
+const MINE = 'M';
+const EXPLOSION = 'E';
+
+export {
+  EXPLOSION,
+  MINE,
+  STATUS
+};
