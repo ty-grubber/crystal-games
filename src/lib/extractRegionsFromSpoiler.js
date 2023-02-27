598887fa504e9a617d376eccac9aad57e02ce899
Tyler Grubb <grubber1618@gmail.com>
2023-02-27 13:25:58 -0600

    fix: bug fixes for matching multiple key items

--- src/lib/extractRegionsFromSpoiler.js
+++ src/lib/extractRegionsFromSpoiler.js
@@ -15,13 +15,16 @@ function extractRegionsFromSpoiler(spoilerFileText) {
   const solutionStartIndex = spoilerLines.findIndex(line => line.includes('Solution:'));
   const solutionEndIndex = spoilerLines.findIndex(line => line.includes('Useless Stuff:'));
   const solutionLines = `${spoilerLines.slice(solutionStartIndex, solutionEndIndex).join(';')};`;
-  const uselessStuffLines = spoilerLines.slice(
+  const uselessStuffLines = `${spoilerLines.slice(
     solutionEndIndex,
     spoilerLines.findIndex(line => line.includes('Xtra Stuff:')),
-  ).join(';');
+  ).join(';')};`;
 
   KEY_ITEMS.forEach(item => {
-    let matchedRegionIds;
+    /**
+     * @type {(number | undefined)[]}
+     */
+    let matchedRegionIds = [];
     let itemSpoilerLines;
 
     // Search through solution lines first
@@ -31,33 +34,34 @@ function extractRegionsFromSpoiler(spoilerFileText) {
     if (itemSpoilerLines) {
       // Extract region from part after colon of solution line
       const [, location] = itemSpoilerLines.split(':');
-      matchedRegionIds = [REGIONS.find(region => (
+      matchedRegionIds.push(REGIONS.find(region => (
         region.locations.filter(l => location.includes(l)).length > 0 ||
         region.routes.filter(r => location.includes(`Route ${r.toString()} `)).length > 0
-      ))?.id];
-    } else {
-      // Check if item is in useless stuff
-      const uselessRegExp = new RegExp(`;[^;]+${item.name.replace(' ', '_').toUpperCase()}`, 'g');
-      itemSpoilerLines = uselessStuffLines.match(uselessRegExp);
+      ))?.id);
+    }
 
-      if (itemSpoilerLines) {
-        matchedRegionIds = [];
-        for (var i = 0; i < itemSpoilerLines.length; i++) {
-          const [location] = itemSpoilerLines[i].split(':');
-          matchedRegionIds.push(REGIONS.find(region => (
-            region.locations.filter(l => location.includes(l)).length > 0 ||
-            region.routes.filter(r => location.includes(r.toString())).length > 0
-          ))?.id);
+    // Check if item is also in useless stuff
+    const uselessRegExp = new RegExp(`;[^;]+${item.name.replace(' ', '_').toUpperCase()};`, 'g');
+    itemSpoilerLines = uselessStuffLines.match(uselessRegExp);
 
-          // If we have duplicate key items, make sure we note extra ones
-          if (i >= 1) {
-            extraItems.push(item);
-          }
-        }
-      } else {
-        // Item is in its vanilla location
-        matchedRegionIds = [item.vanillaRegion];
+    if (itemSpoilerLines) {
+      for (var i = 0; i < itemSpoilerLines.length; i++) {
+        const [location] = itemSpoilerLines[i].split(':');
+        matchedRegionIds.push(REGIONS.find(region => (
+          region.locations.filter(l => location.includes(l)).length > 0 ||
+          region.routes.filter(r => location.includes(r.toString())).length > 0
+        ))?.id);
       }
+
+      // If we have duplicate key items, make sure we note extra ones
+      for (var j = 1; j < matchedRegionIds.length; j++) {
+        extraItems.push(item);
+      }
+    }
+
+    if (matchedRegionIds.length === 0) {
+      // Item is in its vanilla location
+      matchedRegionIds.push(item.vanillaRegion);
     }
 
     matchedRegionIds.forEach(matchedId => {
