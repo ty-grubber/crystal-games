ac141cacf19f2e16cfe61168c5457aeb0405d71a
Tyler Grubb <grubber1618@gmail.com>
2023-03-08 18:10:56 -0600

    fix: remove unneeded import

--- src/routes/points/+page.svelte
+++ src/routes/points/+page.svelte
@@ -7,7 +7,6 @@
 	import extractRegionsFromSpoiler from '$lib/extractRegionsFromSpoiler';
 	import REGIONS from '../../constants/regions';
 	import KEY_ITEMS, { BLUE_CARD_KEY_ITEM, COIN_CASE_KEY_ITEM, KEY_ITEMS_3PTS, KEY_ITEMS_5PTS, KEY_ITEMS_7PTS, KEY_ITEMS_9PTS } from '../../constants/keyItems';
-	import { stop_propagation } from 'svelte/internal';
 
   const availableItemsPointCellStyles = "width: 65px !important; text-align: center; font-size: 24px;";
   const availableItemsItemCellStyles = "padding: 0; width: 300px; white-space: normal;"
