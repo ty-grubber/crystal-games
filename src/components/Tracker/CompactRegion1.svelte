096afe99710f559b0b74bf9957f24892a8a6eb3c
Tyler Grubb <grubber1618@gmail.com>
2024-01-11 21:23:45 -0600

    refactor: Consolidate both points pages into one at /points; improve settings dialog spacing

--- src/components/Tracker/CompactRegion1.svelte
+++ src/components/Tracker/CompactRegion1.svelte
@@ -223,6 +223,7 @@
   <div class="region-boxes">
     {#each regionPoints as rp, i (rp.regionId)}
     {@const missingSolutionItems = showSolution ? getRemainingSolutionItems(i) : []}
+      <!-- svelte-ignore a11y-no-static-element-interactions -->
       <div
         class="box"
         class:hovering={hoveringOverBasket === `${baskets[i].type}_${baskets[i].name}`}
@@ -257,6 +258,7 @@
         </div>
         <div class={`items ${baskets[i].items.concat(missingSolutionItems).length > 11 ? 'big-list' : ''}`}>
           {#each baskets[i].items as item, itemIndex (`${item.id}_${itemIndex}`)}
+            <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
             <img
               src={`/keyItems/${item.id}.png`}
               alt={item.name}
@@ -302,6 +304,7 @@
       <div class="point-label">{itemBasket.items[0].points}</div>
       <div class="item-row">
         {#each itemBasket.items as item, itemIndex (`${item.id}_${itemIndex}`)}
+          <!-- svelte-ignore a11y-no-static-element-interactions -->
           <div
             class="item-wrapper"
             class:draggable={!item.regionFound || item.regionFound === 'P'}
--- src/routes/points/+page.svelte
+++ src/routes/points/+page.svelte
@@ -1,267 +1,145 @@
 <script>
-// @ts-nocheck
-  import extractRegionsFromSpoiler from '$lib/extractFromSpoiler';
-  import { randomizeArray, randomTiesSorting } from '$lib/randomize';
-  import Button, { Label } from '@smui/button';
-  import Dialog, { Actions, Content, Title } from '@smui/dialog';
-  import Select, { Option } from '@smui/select';
-  import CustomPtsDialog from '../../components/CustomPts.svelte';
-  import KEY_ITEMS from '../../constants/keyItemPresets';
-	import CompactRegion1 from '../../components/Tracker/CompactRegion1.svelte';
-	import ClassicRegion from '../../components/Tracker/ClassicRegion.svelte';
-	import PointsHintDialog from '../../components/HowTos/PointsHintDialog.svelte';
+  // @ts-nocheck
+    import checkToExposeRegion from '$lib/checkToExposeRegion';
+    import { extractPointsInfoFromSpoiler } from '$lib/extractFromSpoiler';
+    import Button, { Label } from '@smui/button';
+    import Dialog, { Actions, Content, Title } from '@smui/dialog';
+    import Tab, { Label as TabLabel } from '@smui/tab';
+    import TabBar from '@smui/tab-bar';
+    import CustomPtsDialog from '../../components/CustomPts.svelte';
+    import PointsHintDialog from '../../components/HowTos/PointsHintDialog.svelte';
+    import PointsSharedSettings from '../../components/PointsSharedSettings.svelte';
+    import ClassicRegion from '../../components/Tracker/ClassicRegion.svelte';
+    import CompactRegion1 from '../../components/Tracker/CompactRegion1.svelte';
+    import KEY_ITEMS from '../../constants/keyItemPresets';
 
-  /**
-	 * @type {{ regionId: number; points: number; items: any[]; }[]}
-	 */
-  let regionPoints;
+    let regionPoints;
 
-  let baskets = [];
-  let regionRevealOrder = [];
-  let revealedRegions = [];
-  let showSolution = false;
-  let howToDialogOpen = false;
-  let inGameMenuOpen = false;
-  let customPtsMenuOpen = false;
+    let baskets = [];
+    let regionRevealOrder = [];
+    let revealedRegions = [];
+    let showSolution = false;
 
-  let settingsDialogOpen = true;
-  let revealRegionPoints = false;
-  let trackerLayout = 'classic';
-  let revealOrdering = 'random';
-  let initialRevealedRegions = 1;
-  let spoilerFile;
+    let settingsDialogOpen = true;
+    let howToDialogOpen = false;
+    let customPtsMenuOpen = false;
+    let inGameMenuOpen = false;
+    let activeTab = 'Solo';
 
-  let selectedAvailableItem = {};
-  let selectedFoundItem = {};
+    let revealRegionPoints = false;
+    let trackerLayout;
+    let revealOrdering;
+    let initialRevealedRegions;
+    let spoilerFile;
 
-  let keyItems = [ ...KEY_ITEMS];
-  let keyItemPointValues = [9, 7, 5, 3];
+    let keyItems = [ ...KEY_ITEMS];
+    let keyItemPointValues = [9, 7, 5, 3];
 
-  /**
-	 * @param {any} e
-	 */
-  async function handleSpoilerFileChange(e) {
-    spoilerFile = e.target.files[0];
-  };
-
-  async function onStartClick() {
-    const file = spoilerFile;
-    if (file != null) {
-      const spoilerText = await file.text();
-
-      const extraction = extractRegionsFromSpoiler(spoilerText, keyItems);
-      regionPoints = extraction.regionPointsArray;
-
-      keyItemPointValues = [
-        ...new Set(extraction.randomizedItems.map(item => item.points)),
-      ].sort((a, b) => b - a);
-
-      // Make our starting baskets
-      const newBaskets = [
-        { type: 'region', name: '1', items: []},
-        { type: 'region', name: '2', items: []},
-        { type: 'region', name: '3', items: []},
-        { type: 'region', name: '4', items: []},
-        { type: 'region', name: '5', items: []},
-        { type: 'region', name: '6', items: []},
-        { type: 'region', name: '7', items: []},
-        { type: 'region', name: '8', items: []},
-        { type: 'region', name: '9', items: []},
-        { type: 'region', name: '10', items: []},
-        { type: 'region', name: '11', items: []},
-        { type: 'region', name: '12', items: []},
-        { type: 'region', name: '13', items: []},
-        { type: 'region', name: '14', items: []},
-        { type: 'region', name: '15', items: []},
-        { type: 'region', name: '16', items: []},
-      ];
-
-      const itemBaskets = keyItemPointValues.map(pointValue => ({
-        type: 'item',
-        name: pointValue.toString(),
-        items: extraction.randomizedItems.filter(item => item.points === pointValue),
-      }));
-
-      baskets = newBaskets.concat(itemBaskets);
-      let regionsWithTotalPoints = regionPoints.map(region => ({ id: region.regionId, points: region.points }));
-
-      const rngSeed = extraction.rngSeed || file.name;
-      switch (revealOrdering) {
-        case 'random':
-          regionsWithTotalPoints = randomizeArray(regionsWithTotalPoints, rngSeed);
-        default:
-          regionsWithTotalPoints = randomTiesSorting(regionsWithTotalPoints, revealOrdering, rngSeed);
-      }
-      regionRevealOrder = regionsWithTotalPoints.map(r => r.id);
-      revealedRegions = regionRevealOrder.splice(0, initialRevealedRegions);
-      settingsDialogOpen = false;
-    }
-  }
-
-  function handleStartNewGame() {
-    if(confirm('Starting a new game will end the current one. Are you sure you wish to start a new game?')) {
-      spoilerFile = null;
-      regionPoints = null;
-      baskets = [];
-      regionRevealOrder = [];
-      revealedRegions = [];
-      selectedAvailableItem = {};
-      selectedFoundItem = {};
-      revealOrdering = 'random';
-      showSolution = false;
-      revealRegionPoints = false;
-      initialRevealedRegions = 1;
-      howToDialogOpen = false;
-      inGameMenuOpen = false;
+    function openSettingsDialog() {
       settingsDialogOpen = true;
     }
-  }
 
-  function handleShowSolution() {
-    showSolution = !showSolution;
-  }
+    function openHowToDialog() {
+      howToDialogOpen = true;
+    }
 
-  function toggleRevealAllRegions() {
-    revealRegionPoints = !revealRegionPoints;
-  }
+    function openCustomPointsDialog() {
+      customPtsMenuOpen = true;
+    }
 
-  function openHowToDialog() {
-    howToDialogOpen = true;
-  }
+    function openInGameMenu() {
+      inGameMenuOpen = true;
+    }
 
-  function openSettingsDialog() {
-    settingsDialogOpen = true;
-  }
+    function handleShowSolution() {
+      showSolution = !showSolution;
+    }
 
-  function openInGameMenu() {
-    inGameMenuOpen = true;
-  }
+    function toggleRevealAllRegions() {
+      revealRegionPoints = !revealRegionPoints;
+    }
 
-  function checkToExposeRegion(originalBasket, targetBasket, movedItem) {
-    if (regionRevealOrder.length > 0) {
-      if (originalBasket.type === 'item' && targetBasket.type === 'region' && movedItem.points === 9) {
-        // Found a badge in a region so expose a region's point value
-        const regionToExpose = regionRevealOrder.shift();
-        revealedRegions.unshift(regionToExpose);
+    function handleUpdatePointValues(updatedKeyItems) {
+      keyItems = [...updatedKeyItems]
+      customPtsMenuOpen = false;
+    }
 
-        regionRevealOrder = regionRevealOrder;
-        revealedRegions = revealedRegions;
-      } else if (originalBasket.type === 'region' && targetBasket.type === 'item' && movedItem.points === 9) {
-        // Unmarked a badge in a region so hide the last exposed region's point value
-        const regionToExpose = revealedRegions.shift();
-        regionRevealOrder.unshift(regionToExpose);
+    async function onStartClick() {
+      if (spoilerFile != null) {
+        ({
+          baskets,
+          keyItemPointValues,
+          regionRevealOrder,
+          regionPoints
+        } = await extractPointsInfoFromSpoiler(spoilerFile, keyItems, revealOrdering));
 
-        regionRevealOrder = regionRevealOrder;
-        revealedRegions = revealedRegions;
+        revealedRegions = regionRevealOrder.splice(0, initialRevealedRegions);
+        settingsDialogOpen = false;
       }
     }
-  }
 
-  function handleOutsideRegionTableClick(e) {
-    if (
-      e.explicitOriginalTarget.tagName.toLowerCase() !== 'img' &&
-      e.explicitOriginalTarget.parentElement.tagName.toLowerCase() !== 'button' &&
-      !keyItemPointValues.find(value => value.toString() === e.explicitOriginalTarget.innerHTML)
-    ) {
-      selectedAvailableItem = {};
-      selectedFoundItem = {};
+    function handleSettingsSubmit(settings) {
+      ({
+        trackerLayout,
+        revealOrdering,
+        initialRevealedRegions,
+        spoilerFile
+      } = settings);
+      onStartClick();
     }
-  }
 
-  function handleUpdatePointValues(updatedKeyItems) {
-    keyItems = [...updatedKeyItems];
-    customPtsMenuOpen = false;
-  }
-
-  function beforeUnload(event) {
-    if (regionPoints) {
-      event.preventDefault();
-      return (event.returnValue = '');
+    function handleStartNewGame() {
+      if(confirm('Starting a new game will end the current one. Are you sure you wish to start a new game?')) {
+        spoilerFile = null;
+        regionPoints = null;
+        baskets = [];
+        regionRevealOrder = [];
+        revealedRegions = [];
+        revealOrdering = 'random';
+        showSolution = false;
+        revealRegionPoints = false;
+        initialRevealedRegions = 1;
+        howToDialogOpen = false;
+        inGameMenuOpen = false;
+        settingsDialogOpen = true;
+      }
     }
-  }
-</script>
 
-<svelte:window on:beforeunload={beforeUnload} />
+    function handleCheckToExposeRegion(originalBasket, targetBasket, movedItem) {
+      ({ regionRevealOrder, revealedRegions } = checkToExposeRegion(
+        originalBasket.type,
+        targetBasket.type,
+        movedItem.points,
+        regionRevealOrder,
+        revealedRegions
+      ));
+    }
 
-<svelte:head>
-  <title>Pokémon Crystal Points Hint Tracker</title>
-  <meta property="description" content="Streamline your randomizer by using a key item region point hint system!" />
-  {#each KEY_ITEMS as keyItem}
-    <link rel="preload" as="image" href={`/keyItems/${keyItem.id}.png`} />
-  {/each}
-  <link rel="preload" as="image" href="/maps/johto-points-region-map.png" />
-  <link rel="preload" as="image" href="/maps/kanto-points-region-map.png" />
-</svelte:head>
+    function beforeUnload(event) {
+      if (regionPoints) {
+        event.preventDefault();
+        return (event.returnValue = '');
+      }
+    }
+  </script>
 
-<div class="page">
-  {#if !regionPoints}
-    <h1>Pokémon Crystal Points Tracker</h1>
-    <Button color="primary" on:click={openSettingsDialog} variant="raised">
-      <Label>New Game</Label>
-    </Button>
-    <Button color="secondary" on:click={openHowToDialog} variant="raised">
-      <Label>How To Play</Label>
-    </Button>
-    <Button color="secondary" href="/" variant="outlined">
-      <Label>Games Home</Label>
-    </Button>
-    <br /><br />
-  {/if}
+  <svelte:window on:beforeunload={beforeUnload} />
 
-  <Dialog bind:open={settingsDialogOpen}>
-    <Title id="settingsTitle">Pokémon Crystal Points Hint Tracker Settings</Title>
-    <Content id="settingsContent">
-      <label for="spoiler">
-        {!spoilerFile ? 'Upload spoiler file (.txt):' : 'Spoiler Uploaded!'}
-      </label>
-      <input
-        id="spoiler"
-        accept=".txt"
-        type="file"
-        on:change={handleSpoilerFileChange}
-        style="margin-right: 1rem;"
-      />
-      <br /><br />
-      <Select bind:value={trackerLayout} variant="outlined" label="Tracker Layout Style" style="width: 220px;">
-        <Option value="classic">Classic</Option>
-        <Option value="compact1">Compact</Option>
-      </Select>
-      <div class="tracker-blurb">
-        <span>
-          {#if trackerLayout === 'classic'}
-            A wider layout with all references to help you complete a seed.
-          {:else}
-            A compact layout for a runner more familiar with the regions and point values.
-          {/if}
-        </span>
-      </div>
-      <br /><br />
-      <Select bind:value={revealOrdering} variant="outlined" label="Region Reveal Order" style="width: 220px;">
-        <Option value="random">Random</Option>
-        <Option value="desc">Highest First</Option>
-        <Option value="asc">Lowest First</Option>
-      </Select>
-      <br /><br />
-      <label for="startRevealed">Initial Revealed Regions</label>
-      <input
-        id={`startRevealed`}
-        class="start-revealed"
-        type="number"
-        min="0"
-        max="16"
-        bind:value={initialRevealedRegions}
-      />
-      <br /><br />
-      <Button
-        color="primary"
-        variant="outlined"
-        on:click={() => customPtsMenuOpen = true}
-      >
-        Customize Item Points
-      </Button>
-      <br /><br /><br />
-      <Button color="primary" on:click={onStartClick} disabled={!spoilerFile} variant="raised">
-        <Label>Start Game</Label>
+  <svelte:head>
+    <title>Pokémon Crystal Points Hint Tracker Race Connection</title>
+    <meta property="description" content="Host or join a points hint tracker race without sharing the spoiler log to all participants" />
+    {#each KEY_ITEMS as keyItem}
+      <link rel="preload" as="image" href={`/keyItems/${keyItem.id}.png`} />
+    {/each}
+    <link rel="preload" as="image" href="/maps/johto-points-region-map.png" />
+    <link rel="preload" as="image" href="/maps/kanto-points-region-map.png" />
+  </svelte:head>
+
+  <div class="page">
+    {#if !regionPoints}
+      <h1>Pokémon Crystal Points Tracker</h1>
+      <Button color="primary" on:click={openSettingsDialog} variant="raised">
+        <Label>New Game</Label>
       </Button>
       <Button color="secondary" on:click={openHowToDialog} variant="raised">
         <Label>How To Play</Label>
@@ -269,85 +147,89 @@
       <Button color="secondary" href="/" variant="outlined">
         <Label>Games Home</Label>
       </Button>
-    </Content>
-  </Dialog>
-
-  <CustomPtsDialog bind:isOpen={customPtsMenuOpen} onConfirmPts={handleUpdatePointValues} />
-
-  <Dialog bind:open={inGameMenuOpen} surface$style="width: 450px">
-    <Title id="inGameMenuTitle">Tracker Menu</Title>
-    <Content id="inGameMenuContent">
-      <Button color="primary" variant="outlined" on:click={toggleRevealAllRegions}>
-        <Label>{revealRegionPoints ? 'Hide' : 'Show'} Region Points</Label>
-      </Button>
-      <t />
-      <Button color="primary" variant="outlined" on:click={handleShowSolution}>
-        <Label>{showSolution ? 'Hide' : 'Show'} Solution</Label>
-      </Button>
       <br /><br />
-      <Button color="secondary" on:click={openHowToDialog} variant="raised">
-        <Label>How To Play</Label>
-      </Button>
-      <br /><br />
-      <Button color="primary" on:click={handleStartNewGame} variant="raised">
-        <Label>Start New Game</Label>
-      </Button>
-      <Actions>
-        <Button>Close</Button>
-      </Actions>
-    </Content>
-  </Dialog>
-
-  <PointsHintDialog bind:isOpen={howToDialogOpen} />
-
-  {#if regionPoints?.length > 0}
-    {#if trackerLayout === 'compact1'}
-      <CompactRegion1
-        bind:baskets
-        spoilerFile={spoilerFile}
-        regionPoints={regionPoints}
-        revealedRegions={revealedRegions}
-        revealRegionPoints={revealRegionPoints}
-        bind:selectedAvailableItem
-        bind:selectedFoundItem
-        showSolution={showSolution}
-        handleOutsideRegionTableClick={handleOutsideRegionTableClick}
-        checkToExposeRegion={checkToExposeRegion}
-        openInGameMenu={openInGameMenu}
-      />
-    {:else}
-      <ClassicRegion
-        spoilerFile={spoilerFile}
-        regionPoints={regionPoints}
-        bind:baskets
-        revealedRegions={revealedRegions}
-        bind:selectedAvailableItem
-        bind:selectedFoundItem
-        showSolution={showSolution}
-        revealRegionPoints={revealRegionPoints}
-        handleOutsideRegionTableClick={handleOutsideRegionTableClick}
-        checkToExposeRegion={checkToExposeRegion}
-        openInGameMenu={openInGameMenu}
-      />
     {/if}
-  {/if}
-</div>
 
-<style>
-  input.start-revealed {
-    font-size: 1.5rem;
-    height: 40px;
-    margin-left: 20px;
-    text-align: center;
-    width: 70px;
-  }
+    <Dialog bind:open={settingsDialogOpen}>
+      <Title id="settingsTitle">Pokémon Crystal Points Hint Tracker Settings</Title>
+      <Content id="settingsTabbedContent">
+        <TabBar tabs={['Solo', 'Host', 'Join']} let:tab bind:active={activeTab}>
+          <Tab {tab}>
+            <TabLabel>{tab}</TabLabel>
+          </Tab>
+        </TabBar>
+        {#if activeTab === 'Host'}
+          <PointsSharedSettings
+            onSubmit={handleSettingsSubmit}
+            openHowToDialog={openHowToDialog}
+            openCustomPointsDialog={openCustomPointsDialog}
+            showNetworking={true}
+          />
+        {:else if activeTab === 'Solo'}
+          <PointsSharedSettings
+            onSubmit={handleSettingsSubmit}
+            openHowToDialog={openHowToDialog}
+            openCustomPointsDialog={openCustomPointsDialog}
+          />
+        {:else}
+          This is the Join content
+        {/if}
+      </Content>
+    </Dialog>
 
-  .tracker-blurb {
-    display: inline-block;
-    font-size: 0.8rem;
-    line-height: 1.5;
-    margin-left: 10px;
-    vertical-align: middle;
-    width: 265px;
-  }
-</style>
+    <CustomPtsDialog bind:isOpen={customPtsMenuOpen} onConfirmPts={handleUpdatePointValues} />
+
+    <PointsHintDialog bind:isOpen={howToDialogOpen} />
+
+    <Dialog bind:open={inGameMenuOpen} surface$style="width: 450px">
+      <Title id="inGameMenuTitle">Tracker Menu</Title>
+      <Content id="inGameMenuContent">
+        <Button color="primary" variant="outlined" on:click={toggleRevealAllRegions}>
+          <Label>{revealRegionPoints ? 'Hide' : 'Show'} Region Points</Label>
+        </Button>
+        <t />
+        <Button color="primary" variant="outlined" on:click={handleShowSolution}>
+          <Label>{showSolution ? 'Hide' : 'Show'} Solution</Label>
+        </Button>
+        <br /><br />
+        <Button color="secondary" on:click={openHowToDialog} variant="raised">
+          <Label>How To Play</Label>
+        </Button>
+        <br /><br />
+        <Button color="primary" on:click={handleStartNewGame} variant="raised">
+          <Label>Start New Game</Label>
+        </Button>
+        <Actions>
+          <Button>Close</Button>
+        </Actions>
+      </Content>
+    </Dialog>
+
+    {#if regionPoints?.length > 0}
+      {#if trackerLayout === 'compact1'}
+        <CompactRegion1
+          bind:baskets
+          spoilerFile={spoilerFile}
+          regionPoints={regionPoints}
+          revealedRegions={revealedRegions}
+          revealRegionPoints={revealRegionPoints}
+          showSolution={showSolution}
+          checkToExposeRegion={handleCheckToExposeRegion}
+          openInGameMenu={openInGameMenu}
+          keyItemPointValues={keyItemPointValues}
+        />
+      {:else}
+        <ClassicRegion
+          spoilerFile={spoilerFile}
+          regionPoints={regionPoints}
+          bind:baskets
+          revealedRegions={revealedRegions}
+          showSolution={showSolution}
+          revealRegionPoints={revealRegionPoints}
+          checkToExposeRegion={handleCheckToExposeRegion}
+          openInGameMenu={openInGameMenu}
+          keyItemPointValues={keyItemPointValues}
+        />
+      {/if}
+    {/if}
+  </div>
