8129eb8b5fa85c00388c8cf6c890730ff439a18f
Tyler Grubb <grubber1618@gmail.com>
2026-06-06 12:04:15 -0600

    chore: Add notes for spoiler extraction

--- src/lib/extractFromSpoiler.js
+++ src/lib/extractFromSpoiler.js
@@ -17,7 +17,7 @@ function extractRegionsFromSpoiler(spoilerFileText, keyItems) {
 
   if (spoilerLines.find(line => line.includes('Seed:')) && spoilerLines.find(line => line.includes('CV:'))) {
     // TODO: switch to Kovolta randomizer
-    return extractRegionsFromSpeedchoiceSpoiler(spoilerLines, keyItems);
+    return extractRegionsFromKovoltaSpoiler(spoilerLines, keyItems);
   } else {
     return extractRegionsFromSpeedchoiceSpoiler(spoilerLines, keyItems);
   }
@@ -28,7 +28,62 @@ function extractRegionsFromSpoiler(spoilerFileText, keyItems) {
  * @param {any[]} keyItems
  */
 function extractRegionsFromKovoltaSpoiler(spoilerLines, keyItems) {
-  
+  /**
+   * @type {{ points: number; id: string; name: string; }[]}
+   */
+  let randomizedItems = [];
+  const regionPointsArray = REGIONS.map(region => ({
+    regionId: region.id,
+    name: region.name,
+    description: region.description,
+    points: 0,
+    items: [],
+  }));
+
+  const rngSeed = spoilerLines.find(line => line.includes('Seed:'))?.replace('Seed: ', '');
+
+  // Items are all caps and we need to find the line where the item does not have a : after it
+  // When matching, make sure the line is after the ----- ITEM LOCATIONS -----
+    // This is where we should the start index for the itemLocations lines
+  // Something like: new RegExp(`${item.name.replace('_', '').toUpperCase()}(?!:)`);
+  // Note: item above is the key item constant
+  // Note: only badges remove the space?
+  // Exceptions: Badges, TMs, and HMs (which match item.id not item.name),
+    // So, we could check item.id if we can't find it in item.name, or we could add a property to the constant to indicate the field to check (better, because we do badges with it)
+
+  // And remember that we need to match multiple items in log (ex. Water Stone, TM08)
+
+  // Need to verify that if an item is not randomized (ex. GS Ball), that it doesn't appear in the log
+
+  // If Item is not in a shop, we can just use the line it is found on for its location. Location is at start of line
+  // If Item is in shop, we need to check the line it is found on for its location, which would be at start of line
+  //     If first character found is a | on the line (after trimming), we need to try the previous line and repeat check
+  //        Can we get the index of the line in the array when we use the regex to match above?
+
+  // We need to find the modifiers in the SETTINGS section that trigger certain key items to increase in value
+    // Alternatively, we might just have to look for specific ITEM_LOCATION strings
+      // Ex: (LAKE_OF_RAGE_MAGIKARP_HOUSE_MANS_GIFT_FOR_MAGIKARP, BILLS_HOUSE_BILLS_GRANDPAS_GIFT_FOR_LICKITUNG)
+  // Name -> Speedchoice Modifier -> Kovolta Modifier
+  // Pokedex -> Mon Locked Checks -> Need to check with Kovolta how to know if this is on
+  // Bicycle -> Hidden Items
+  //    Two Options for Kovolta:
+  //      -> Look directly for RANDOMIZE_REGULAR_HIDDEN_ITEMS. If next line is VALUE: true, then true
+  //      -> Look directly for - REGULAR_HIDDEN_ITEMS. If exists, then true
+  // Bicycle -> Start With Bike (which removes it from spoiler log) -> START_WITH_ITEMS ==\n+==> KEY_ITEMS ==\n+==> BICYCLE
+  // Blue Card -> Buena Items -> Need to check with Kovolta how to know if this is on
+  // Coin Case -> Game Corner -> Need to check with Kovolta how to know if this is on
+  // TM12 -> Mon Locked Checks -> See Pokedex above
+
+  // All locations use _ instead spaces. So we need to check ROUTE_42 not ROUTE 42
+  // We might be able to mostly re-use the regions.js constant, but we could do some inline hacks instead of making a new set of constants
+    // Ex. Region 12 needs to add DRAGON_SHRINE_BADGE
+    // But we also have PLAYERS_HOUSE and MR_POKEMONS_HOUSE (instead of Mr. Pokemon), so it might just be better to do a fresh constant (using the existing one as a base)
+
+  return {
+    randomizedItems,
+    regionPointsArray,
+    rngSeed,
+  }
 }
 
 /**
